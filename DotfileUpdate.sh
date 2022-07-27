e199ef26b4ee62da6235669d0a9a537b9f2fdc3e
g2jz <gmartinmendiola@gmail.com>
2022-07-27 15:26:21 +0200

    Path changed

--- DotfileUpdate.sh
+++ DotfileUpdate.sh
@@ -1,20 +1,20 @@
 #! /opt/homebrew/bin/bash
 
 # Update files
-sudo cp -r ~/.vim ~/Repos/dotfiles/.vim
-sudo cp ~/.fzf.zsh ~/Repos/dotfiles/.fzf.zsh
-sudo cp ~/.iterm2_shell_integration.zsh ~/Repos/dotfiles/.iterm2_shell_integration.zsh
-sudo cp ~/.p10k.zsh ~/Repos/dotfiles/.p10k.zsh
-sudo cp ~/.tmux.conf ~/Repos/dotfiles/.tmux.conf
-sudo cp ~/.zsh_plugins.sh ~/Repos/dotfiles/.zsh_plugins.sh
-sudo cp ~/.zshrc ~/Repos/dotfiles/.zshrc
-sudo cp -r ~/.config/karabiner ~/Repos/dotfiles/.config/karabiner
-sudo cp -r ~/.config/neofetch ~/Repos/dotfiles/.config/neofetch
-sudo cp -r ~/.config/nvim ~/Repos/dotfiles/.config/nvim
-sudo cp -r ~/.config/ranger ~/Repos/dotfiles/.config/ranger
+sudo cp -r ~/.vim ~/Github/dotfiles/.vim
+sudo cp ~/.fzf.zsh ~/Github/dotfiles/.fzf.zsh
+sudo cp ~/.iterm2_shell_integration.zsh ~/Github/dotfiles/.iterm2_shell_integration.zsh
+sudo cp ~/.p10k.zsh ~/Github/dotfiles/.p10k.zsh
+sudo cp ~/.tmux.conf ~/Github/dotfiles/.tmux.conf
+sudo cp ~/.zsh_plugins.sh ~/Github/dotfiles/.zsh_plugins.sh
+sudo cp ~/.zshrc ~/Github/dotfiles/.zshrc
+sudo cp -r ~/.config/karabiner ~/Github/dotfiles/.config/karabiner
+sudo cp -r ~/.config/neofetch ~/Github/dotfiles/.config/neofetch
+sudo cp -r ~/.config/nvim ~/Github/dotfiles/.config/nvim
+sudo cp -r ~/.config/ranger ~/Github/dotfiles/.config/ranger
 
 # Git
-cd ~/Repos/dotfiles
+cd ~/Github/dotfiles
 git add .
 git commit -m "Dotfiles Updated"
 git push -u origin main
